e945b7d300edf8917c4518fa1923e43d9bf2a885
Jorge Cortés <jorge.ecro@gmail.com>
2015-08-18 23:26:56 -0500

    First Commit, file initialization

---
+++ dev/prod/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html class="no-js" lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Rizoma Cultural</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+</body>
+</html>
